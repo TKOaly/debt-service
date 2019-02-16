02f5977b980618650c68a6e4ab585fe897d35bd9
alehuo <aleksi.huotala@helsinki.fi>
2019-02-16 15:40:11 +0200

    Enable ESLint, linted code

--- src/views/layouts/default.jsx
+++ src/views/layouts/default.jsx
@@ -1,13 +1,20 @@
-const React = require('react')
+const React = require("react");
 
 module.exports = function DefaultLayout(props) {
-  return (
-    <html>
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" type="text/css" href="/public/main.css" />
-      </head>
-      <body>{props.children}</body>
-    </html>
-  )
-}
+    return (
+        <html>
+            <head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link
+                    rel="stylesheet"
+                    type="text/css"
+                    href="/public/main.css"
+                />
+            </head>
+            <body>{props.children}</body>
+        </html>
+    );
+};
