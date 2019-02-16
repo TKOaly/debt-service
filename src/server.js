d5759a2149f0f45e5f4b172f4f0e34ecf2d8c7a3
Joona Heikkilä <joona.o.heikkila@gmail.com>
2019-02-16 15:21:02 +0200

    Improve layout

--- src/server.js
+++ src/server.js
@@ -9,44 +9,58 @@ const PORT = process.env.PORT || 8080
 
 const app = express()
 app.use(cp())
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jsx');
-app.engine('jsx', require('express-react-views').createEngine());
+app.use('/public', express.static(__dirname + '/public'))
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
 app.get('/', checkLogin, (req, res) => {
   const stmt = db.prepare('SELECT * FROM debt WHERE email = ?')
   stmt.run(req.user.email)
   const arr = []
-  stmt.each((err, row) => {
-    if (err) {
-      console.log(err)
-      res.status(500).send('Internal server error')
+  stmt.each(
+    (err, row) => {
+      if (err) {
+        console.log(err)
+        res.status(500).send('Internal server error')
+      }
+      arr.push(row)
+    },
+    () => {
+      res.render('app', { debt: arr, userScreenName: req.user.screenName })
     }
-    arr.push(row)
-  }, () => {
-    res.render('app', {debt: arr, userScreenName: req.user.screenName})
-  })
+  )
 })
 
 async function checkLogin(req, res, next) {
   const token = req.cookies['token']
   if (!token) {
-    return res.redirect(`${process.env.USER_SERVICE_URL}?serviceIdentifier=${process.env.SERVICE_ID}`)
+    return res.redirect(
+      `${process.env.USER_SERVICE_URL}?serviceIdentifier=${
+        process.env.SERVICE_ID
+      }`
+    )
   }
   const opts = {
     headers: {
-      'Authorization': 'Bearer ' + token,
-      'Service': process.env.SERVICE_ID
+      Authorization: 'Bearer ' + token,
+      Service: process.env.SERVICE_ID
     }
   }
   try {
-    const result = await request(`${process.env.USER_SERVICE_URL}/api/users/me`, opts)
-      .then(JSON.parse)
+    const result = await request(
+      `${process.env.USER_SERVICE_URL}/api/users/me`,
+      opts
+    ).then(JSON.parse)
     req.user = result.payload
-    next() 
-  } catch(e) {
+    next()
+  } catch (e) {
     console.log(e)
-    return res.redirect(`${process.env.USER_SERVICE_URL}?serviceIdentifier=${process.env.SERVICE_ID}`)
+    return res.redirect(
+      `${process.env.USER_SERVICE_URL}?serviceIdentifier=${
+        process.env.SERVICE_ID
+      }`
+    )
   }
 }
 
